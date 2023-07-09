25b130a231fd600487eab2706aa08c82653dd20e
Michal Staniecko <michal.staniecko@gmail.com>
2023-07-09 10:51:33 +0200

    add replace image feature

--- src/team-member/edit.js
+++ src/team-member/edit.js
@@ -4,6 +4,8 @@ import {
 	useBlockProps,
 	RichText,
 	MediaPlaceholder,
+	BlockControls,
+	MediaReplaceFlow,
 } from '@wordpress/block-editor';
 import { isBlobURL, revokeBlobURL } from '@wordpress/blob';
 import { Spinner, withNotices } from '@wordpress/components';
@@ -60,42 +62,56 @@ function Edit( { attributes, setAttributes, noticeOperations, noticeUI } ) {
 	}, [ url ] );
 
 	return (
-		<div { ...useBlockProps() }>
-			{ url && (
-				<div
-					className={ `wp-block-blocks-course-team-member-img ${
-						isBlobURL( url ) && 'is-loading'
-					}` }
-				>
-					<img src={ url } alt={ alt } />
-					{ isBlobURL( url ) && <Spinner /> }
-				</div>
-			) }
-			<MediaPlaceholder
-				icon={ 'admin-users' }
-				onSelect={ onSelectImage }
-				onSelectURL={ onSelectURL }
-				onError={ onErrorImage }
-				accept={ 'image/*' }
-				allowedTypes={ [ 'image' ] }
-				disableMediaButtons={ !! url }
-				notices={ noticeUI }
-			/>
-			<RichText
-				placeholder={ __( 'Member name', 'team-members' ) }
-				tagName={ 'h4' }
-				onChange={ onChangeName }
-				value={ name }
-				allowedFormats={ [] }
-			/>
-			<RichText
-				placeholder={ __( 'Member Bio', 'team-members' ) }
-				tagName={ 'p' }
-				onChange={ onChangeBio }
-				value={ bio }
-				allowedFormats={ [] }
-			/>
-		</div>
+		<>
+			<BlockControls group={ 'inline' }>
+				<MediaReplaceFlow
+					name={ __( 'Replace image', 'team-members' ) }
+					mediaURL={ url }
+					mediaId={ id }
+					onSelect={ onSelectImage }
+					onSelectURL={ onSelectURL }
+					onError={ onErrorImage }
+					accept={ 'image/*' }
+					allowedTypes={ [ 'image' ] }
+				/>
+			</BlockControls>
+			<div { ...useBlockProps() }>
+				{ url && (
+					<div
+						className={ `wp-block-blocks-course-team-member-img ${
+							isBlobURL( url ) && 'is-loading'
+						}` }
+					>
+						<img src={ url } alt={ alt } />
+						{ isBlobURL( url ) && <Spinner /> }
+					</div>
+				) }
+				<MediaPlaceholder
+					icon={ 'admin-users' }
+					onSelect={ onSelectImage }
+					onSelectURL={ onSelectURL }
+					onError={ onErrorImage }
+					accept={ 'image/*' }
+					allowedTypes={ [ 'image' ] }
+					disableMediaButtons={ !! url }
+					notices={ noticeUI }
+				/>
+				<RichText
+					placeholder={ __( 'Member name', 'team-members' ) }
+					tagName={ 'h4' }
+					onChange={ onChangeName }
+					value={ name }
+					allowedFormats={ [] }
+				/>
+				<RichText
+					placeholder={ __( 'Member Bio', 'team-members' ) }
+					tagName={ 'p' }
+					onChange={ onChangeBio }
+					value={ bio }
+					allowedFormats={ [] }
+				/>
+			</div>
+		</>
 	);
 }
 
