4a44dd4bf7ea443fee45a09aeeac788c83dc5185
Michal Staniecko <michal.staniecko@gmail.com>
2023-08-13 22:33:22 +0200

    transform gallery or image block to team members

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType, createBlock } from '@wordpress/blocks';
 import './style.scss';
 import Edit from './edit';
 import save from './save';
@@ -13,4 +13,54 @@ import './team-member';
 registerBlockType( metadata.name, {
 	edit: Edit,
 	save,
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: [ 'core/gallery' ],
+				transform: ( attributes ) => {
+					const innerBlocks = attributes.images.map(
+						( { url, id, alt } ) => {
+							return createBlock( 'blocks-course/team-member', {
+								alt,
+								id,
+								url,
+							} );
+						}
+					);
+					return createBlock(
+						'blocks-course/team-members',
+						{
+							columns: attributes.columns || 2,
+						},
+						innerBlocks
+					);
+				},
+			},
+			{
+				type: 'block',
+				blocks: [ 'core/image' ],
+				isMultiBlock: true,
+				transform: ( attributes ) => {
+					const innerBlocks = attributes.map(
+						( { url, id, alt } ) => {
+							return createBlock( 'blocks-course/team-member', {
+								alt,
+								id,
+								url,
+							} );
+						}
+					);
+					return createBlock(
+						'blocks-course/team-members',
+						{
+							columns:
+								attributes.length > 3 ? 3 : attributes.length,
+						},
+						innerBlocks
+					);
+				},
+			},
+		],
+	},
 } );
